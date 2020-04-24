95f1c1aa6db207dd1d1c40342a90d217eed9f75d
joaobarcia <joaobarcia@gmail.com>
2020-04-24 11:34:19 +0200

    feat(Lang Redirection): Moves language redirection from Netlify to Nuxt

---
+++ middleware/language.js
@@ -0,0 +1,14 @@
+export default function (context) {
+  const currentPath = context.route.path
+  // These paths will not be redirected
+  const explicitPaths = ['/ch/en', '/ch/de']
+
+  // If path is not part of an explicit path
+  if (!explicitPaths.includes(currentPath)) {
+    const lang = context.req.headers['accept-language'].substring(0, 2)
+    // If browser language is set to German, redirect to german page
+    if (lang === 'de') context.redirect(`/ch/de`)
+    // Otherwise redirect to english page
+    else context.redirect(`/ch/en`)
+  }
+}
