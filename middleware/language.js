6f453f34940eae66750441c768602c020aa5fd9b
joaobarcia <joaobarcia@gmail.com>
2020-04-24 11:55:48 +0200

    fix(Middleware safeguard): Only runs middleware code during page request

--- middleware/language.js
+++ middleware/language.js
@@ -1,14 +1,17 @@
 export default function (context) {
-  const currentPath = context.route.path
-  // These paths will not be redirected
-  const explicitPaths = ['/ch/en', '/ch/de']
+  // If request exists (safeguard for avoiding generate errors)
+  if (context.req) {
+    const currentPath = context.route.path
+    // These paths will not be redirected
+    const explicitPaths = ['/ch/en', '/ch/de']
 
-  // If path is not part of an explicit path
-  if (!explicitPaths.includes(currentPath)) {
-    const lang = context.req.headers['accept-language'].substring(0, 2)
-    // If browser language is set to German, redirect to german page
-    if (lang === 'de') context.redirect(`/ch/de`)
-    // Otherwise redirect to english page
-    else context.redirect(`/ch/en`)
+    // If path is not part of an explicit path
+    if (!explicitPaths.includes(currentPath)) {
+      const lang = context.req.headers['accept-language'].substring(0, 2)
+      // If browser language is set to German, redirect to german page
+      if (lang === 'de') context.redirect(`/ch/de`)
+      // Otherwise redirect to english page
+      else context.redirect(`/ch/en`)
+    }
   }
 }
